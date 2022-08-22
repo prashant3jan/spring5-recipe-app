6ffeb4743a9a6a8cfb407af0b751f7b6a5936021
prashant3jan <prashant3jan@gmail.com>
2022-08-22 17:59:01 +0530

    adding one-to-many jpa example

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -1,6 +1,7 @@
 package guru.springframework.domain;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Recipe {
@@ -16,6 +17,10 @@ public class Recipe {
     private String directions;
     //todo
     //private Difficulty difficulty
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
+
     @Lob
     private Byte[] image;
     @OneToOne(cascade = CascadeType.ALL)
@@ -100,4 +105,12 @@ public class Recipe {
     public void setNotes(Notes notes) {
         this.notes = notes;
     }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
 }
