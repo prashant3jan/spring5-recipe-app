69042531f0da54813dc9bc0a42d36cc06911fa5d
prashant3jan <prashant3jan@gmail.com>
2022-08-26 12:46:49 +0530

    adding Query Method examples

--- src/main/java/guru/springframework/repositories/CategoryRepository.java
+++ src/main/java/guru/springframework/repositories/CategoryRepository.java
@@ -2,6 +2,10 @@ package guru.springframework.repositories;
 
 import guru.springframework.domain.Category;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+@Repository
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+    Optional<Category> findByDescription(String Description);
 }
