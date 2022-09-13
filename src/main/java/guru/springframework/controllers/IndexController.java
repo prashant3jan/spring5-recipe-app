94774b1c2009db68776c1f6c192f242bb939dbfe
prashant3jan <prashant3jan@gmail.com>
2022-09-13 17:51:24 +0530

    refactoring for project lombok, assignment completion

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,16 +1,11 @@
 package guru.springframework.controllers;
 
-import guru.springframework.domain.Category;
-import guru.springframework.domain.UnitOfMeasure;
-import guru.springframework.repositories.CategoryRepository;
-import guru.springframework.repositories.UnitOfMeasureRepository;
 import guru.springframework.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Optional;
-
+@Slf4j
 @Controller
 public class IndexController {
     private final RecipeService recipeService;
@@ -21,6 +16,7 @@ public class IndexController {
 
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(Model model){
+        log.debug("Getting Index Page");
         model.addAttribute("recipes", recipeService.getRecipes());
         return "index";
     }
--- src/main/java/guru/springframework/domain/Notes.java
+++ src/main/java/guru/springframework/domain/Notes.java
@@ -1,7 +1,9 @@
 package guru.springframework.domain;
 
+import lombok.*;
 import javax.persistence.*;
 
+@Data
 @Entity
 public class Notes {
 
@@ -13,23 +15,4 @@ public class Notes {
     @Lob
     private String recipeNotes;
 
-    public Recipe getRecipe() {
-        return recipe;
-    }
-
-    public void setRecipe(Recipe recipe) {
-        this.recipe = recipe;
-    }
-
-    public String getRecipeNotes() {
-        return recipeNotes;
-    }
-
-    public void setRecipeNotes(String recipeNotes) {
-        this.recipeNotes = recipeNotes;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
