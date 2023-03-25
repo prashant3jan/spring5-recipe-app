6ee56814255c03578edb36b1f25d352af4b794e8
prashant3jan <prashant3jan@gmail.com>
2023-03-25 10:47:02 +0530

    adding pages and tests for display of recipe

--- src/main/java/guru/springframework/services/RecipeServiceImpl.java
+++ src/main/java/guru/springframework/services/RecipeServiceImpl.java
@@ -6,17 +6,24 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 @Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
-
     private final RecipeRepository recipeRepository;
-
     public RecipeServiceImpl(RecipeRepository recipeRepository) {
         this.recipeRepository = recipeRepository;
     }
+    @Override
+    public Recipe findById(Long l) {
+        Optional<Recipe> recipeOptional = recipeRepository.findById(l);
+        if (!recipeOptional.isPresent()) {
+            throw new RuntimeException("Recipe Not Found!");
+        }
 
+        return recipeOptional.get();
+    }
     @Override
     public Set<Recipe> getRecipes() {
         log.debug("I'm in the service");
