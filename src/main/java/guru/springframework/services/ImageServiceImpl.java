1c6c449be3a4d5b3951267f7a6eb89b96d894646
Prashant Singh <prashant3jan@gmail.com>
2023-04-18 12:53:57 +0530

    saving image to db

--- src/main/java/guru/springframework/services/ImageServiceImpl.java
+++ src/main/java/guru/springframework/services/ImageServiceImpl.java
@@ -1,14 +1,40 @@
 package guru.springframework.services;
 
+import guru.springframework.domain.Recipe;
+import guru.springframework.repositories.RecipeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
 @Service
 @Slf4j
 public class ImageServiceImpl implements ImageService {
+    private RecipeRepository recipeRepository;
+
+    public ImageServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
     @Override
     public void saveImageFile(Long id, MultipartFile file) {
-        log.debug("Received a file");
+        try {
+            log.debug("Received a file");
+            Recipe recipe = recipeRepository.findById(id).get();
+            Byte[] bytes = new Byte[file.getBytes().length];
+
+            int i = 0;
+
+            for (byte b : file.getBytes()) {
+                bytes[i++] = b;
+            }
+            recipe.setImage(bytes);
+            recipeRepository.save(recipe);
+        }catch (IOException e){
+            log.error("Error Occurred " + e);
+            e.printStackTrace();
+        }
+
     }
 }
