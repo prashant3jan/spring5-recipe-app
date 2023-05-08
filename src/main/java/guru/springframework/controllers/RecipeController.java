4724ae3cd7c1fffec55406ecae611f6f957c1339
Prashant Singh <prashant3jan@gmail.com>
2023-05-08 14:50:47 +0530

    adding assignment for NumberFormatException

--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -59,5 +59,16 @@ public class RecipeController {
 
         return modelAndView;
     }
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(NumberFormatException.class)
+    public ModelAndView handleNumberFormat(Exception exception){
+        log.error("Bad request");
+        log.error(exception.getMessage());
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("400error");
+        modelAndView.addObject("exception",exception);
+        return modelAndView;
+    }
 
 }
